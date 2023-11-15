c72f09ec7add51e53973d7bcefc0ed4d01c42f66
Jeremiakim <jeremikimzu67@gmail.com>
2023-11-15 12:11:30 +0700

    Google Login Success

---
+++ client/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { GoogleLogin } from "@react-oauth/google";
+
+const Home = () => {
+  return (
+    <>
+      <p>Home</p>
+    </>
+  );
+};
+
+export default Home;
---
+++ client/routers/index.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+    loader: async () => {
+      if (localStorage.access_token) {
+        return redirect("/");
+      }
+      return null;
+    },
+  },
+  {
+    path: "/",
+    element: <Home />,
+    loader: async () => {
+      if (!localStorage.access_token) {
+        return redirect("/login");
+      }
+      return null;
+    },
+  },
+]);
+
+export default router;
