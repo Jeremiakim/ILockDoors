59d8e482c4d479007bb542f26cae4f3828787772
Jeremiakim <jeremikimzu67@gmail.com>
2023-11-17 17:06:46 +0700

    deploy server

--- client/static.js
+++ client/static.js
@@ -1,2 +1,2 @@
-export const urlName = `http://localhost:3000/`;
-// export const urlName = `https://jkzu.online/`;
+// export const urlName = `http://localhost:3000/`;
+export const urlName = `https://jkzu.online/`;
